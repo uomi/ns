732aaaefc0a054810acdbd46abb3b050a76e491e
uomi <531393@gmail.com>
2016-09-12 21:35:17 +0300

    Add files via upload

---
+++ dnsparse.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func unpackDns(msg []byte, dnsType uint16) (domain string, id uint16, cname string) {
+    d := new(dnsMsg)
+    if !d.Unpack(msg) {
+        return
+    }
+
+    id = d.id
+
+    if len(d.question) < 1 {
+        return
+    }
+
+    domain = d.question[0].Name
+    if len(domain) < 1 {
+        return
+    }
+
+    _, addrs, err := answer(domain, "server", d, dnsType)
+    if err == nil {
+        cname = addrs[0].(*dnsRR_CNAME).Cname
+    }
+    return
+}
+
+func packDns(domain string, id uint16, dnsType uint16) []byte {
+
+    out := new(dnsMsg)
+    out.id = id
+    out.recursion_desired = true
+    out.question = []dnsQuestion{
+        {domain, dnsType, dnsClassINET},
+    }
+
+    msg, ok := out.Pack()
+    if !ok {
+        fmt.Fprintf(os.Stderr, "can't pack domain %s\n", domain)
+        os.Exit(1)
+    }
+    return msg
+}
